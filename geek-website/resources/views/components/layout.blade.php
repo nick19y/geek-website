b54409adf04f5086e7f03340a3299f84f367056f
nick19y <godoi.nic.nic@gmail.com>
2024-08-02 11:08:24 -0300

    finalização da função de login, cadastro e configuração de middleware

--- geek-website/resources/views/components/layout.blade.php
+++ geek-website/resources/views/components/layout.blade.php
@@ -10,7 +10,14 @@
     <nav class="navbar navbar-expand-lg navbar-light bg-light">
         <div class="container-fluid">
             <a class="navbar-brand" href="{{route('series.index')}}">Home</a>
+            @auth
             <a href="{{route('logout')}}">Sair</a>
+            <!-- se envolver o item com essa diretiva do blade, o componente só é mostrado se o usuário estiver logado -->
+            @endauth
+            @guest
+            <a class="navbar-brand" href="{{route('login')}}">Entrar</a>
+            <!-- se envolver o item com essa diretiva do blade, o componente só é mostrado se o usuário não estiver logado -->
+            @endguest
         </div>
     </nav>
     <div class="container">
--- geek-website/routes/web.php
+++ geek-website/routes/web.php
@@ -10,46 +10,54 @@ use App\Http\Controllers\LoginController;
 use App\Http\Controllers\UsersController;
 use App\Http\Middleware\Authenticator;
 
-Route::get('/', function () {
-    return view('index');
-})->middleware(Authenticator::class);
 
-Route::controller(GamesController::class)->group(function(){
-    Route::get('/games','index')->name('games.index');
-    Route::get('/games/create','create')->name('games.create');
-    Route::post('/games/save','store')->name('games.store');
-    Route::delete('/games/destroy/{games}', 'destroy')->name('games.destroy');
-    Route::get('/games/edit/{game}', 'edit')->name('games.edit');
-    Route::put('/games/update/{game}', 'update')->name('games.update');
+Route::middleware(Authenticator::class)->group(function(){
+    Route::get('/series/{series}/seasons', [SeasonsController::class, 'index'])->name('seasons.index');
+
+
+    Route::get('/seasons/{season}/episodes', [EpisodesController::class, 'index'])->name('episodes.index');
+    Route::put('/seasons/{season}/episodes', [EpisodesController::class, 'update'])->name('episodes.update');
+
+    Route::get('/', function () {
+        return view('index');
+    })->middleware(Authenticator::class);
+    
+    Route::controller(GamesController::class)->group(function(){
+        Route::get('/games','index')->name('games.index');
+        Route::get('/games/create','create')->name('games.create');
+        Route::post('/games/save','store')->name('games.store');
+        Route::delete('/games/destroy/{games}', 'destroy')->name('games.destroy');
+        Route::get('/games/edit/{game}', 'edit')->name('games.edit');
+        Route::put('/games/update/{game}', 'update')->name('games.update');
+    });
+    
+    Route::controller(BooksController::class)->group(function(){
+        Route::get('/books', 'index')->name('books.index');
+        Route::get('/books/create','create')->name('books.create');
+        Route::post('/books/save','store')->name('books.store');
+        Route::delete('/books/destroy/{books}', 'destroy')->name('books.destroy');
+        Route::get('/books/edit/{book}', 'edit')->name('books.edit');
+        Route::put('/books/update/{book}', 'update')->name('books.update');
+    });
+    
+    Route::controller(SeriesController::class)->group(function(){
+        Route::get('/series', 'index')->name('series.index');
+        Route::get('/series/create','create')->name('series.create');
+        Route::post('/series/save','store')->name('series.store');
+        Route::delete('/series/destroy/{series}', 'destroy')->name('series.destroy');
+        Route::get('/series/edit/{serie}', 'edit')->name('series.edit');
+        Route::put('/series/update/{serie}', 'update')->name('series.update');
+    });
 });
 
-Route::controller(BooksController::class)->group(function(){
-    Route::get('/books', 'index')->name('books.index');
-    Route::get('/books/create','create')->name('books.create');
-    Route::post('/books/save','store')->name('books.store');
-    Route::delete('/books/destroy/{books}', 'destroy')->name('books.destroy');
-    Route::get('/books/edit/{book}', 'edit')->name('books.edit');
-    Route::put('/books/update/{book}', 'update')->name('books.update');
-});
-
-Route::controller(SeriesController::class)->group(function(){
-    Route::get('/series', 'index')->name('series.index');
-    Route::get('/series/create','create')->name('series.create');
-    Route::post('/series/save','store')->name('series.store');
-    Route::delete('/series/destroy/{series}', 'destroy')->name('series.destroy');
-    Route::get('/series/edit/{serie}', 'edit')->name('series.edit');
-    Route::put('/series/update/{serie}', 'update')->name('series.update');
-});
-
-Route::get('/series/{series}/seasons', [SeasonsController::class, 'index'])->name('seasons.index');
 
 
-Route::get('/seasons/{season}/episodes', [EpisodesController::class, 'index'])->name('episodes.index');
-Route::put('/seasons/{season}/episodes', [EpisodesController::class, 'update'])->name('episodes.update');
+
+Route::get('/register', [UsersController::class, 'create'])->name('users.create');
+Route::post('/register', [UsersController::class, 'store'])->name('users.store');
 
 Route::get('/login', [LoginController::class, 'index'])->name('login');
 Route::post('/login', [LoginController::class, 'store'])->name('singin');
 Route::get('/logout', [LoginController::class, 'destroy'])->name('logout');
 
-Route::get('/register', [UsersController::class, 'create'])->name('users.create');
-Route::post('/register', [UsersController::class, 'store'])->name('users.store');
+// Route::get('/series/{series}/seasons', [SeasonsController::class, 'index'])->name('seasons.index')->middleware(Authenticator::class);
